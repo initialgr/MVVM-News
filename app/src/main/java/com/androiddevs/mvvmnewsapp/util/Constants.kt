9c656ffb5eb0e93032140d9d12e6eafe60c25e5c
Gilang <initial.gr.101@gmail.com>
2020-11-06 10:57:24 +0700

    Finish Commit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "31bbd6080cc245699041068296c00142"
+        const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+
+    }
+}
